8ff8f538102b2cb8e3f4ae55a5216dcff6cc893e
Marquand <marquand.thibault@gmail.com>
2015-11-07 14:39:21 +0100

    Push Configuration USER et suite fiche de frais ...

---
+++ src/PointWeb/FicheBundle/Tests/Controller/AutreControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace PointWeb\FicheBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AutreControllerTest extends WebTestCase
+{
+    /*
+    public function testCompleteScenario()
+    {
+        // Create a new client to browse the application
+        $client = static::createClient();
+
+        // Go to the list view
+        $crawler = $client->request('GET', '/autre/');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /autre/");
+
+        // Go to the show view
+        $crawler = $client->click($crawler->selectLink('show')->link());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code");
+    }
+
+    */
+}
